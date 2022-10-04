f651ff6f6d80115fe5e147cf706bd9d451dcef22
Kylian Leroy <aleexisazul@gmail.com>
2022-10-04 11:13:17 -0300

    setup

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+    return ( 
+    <div>
+        {/* Overlay */}
+        <div>
+            <h2>Heading</h2>
+            <p>Mensaje</p>
+            <button>Libro</button>
+        </div>
+
+    </div>
+     );
+}
+ 
+export default Hero;
