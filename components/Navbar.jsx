f9086d7c3cb71c10b0cc694ec94d7821d9b96aaf
Kylian Leroy <aleexisazul@gmail.com>
2022-10-04 12:44:46 -0300

    navb

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
+    const [color, setColor] = useState('transparent');
+    const [textColor, setTextColor] = useState('white');
+
+    useEffect(() => {
+        const changeColor = () => {
+            if (window.scrollY >= 90) {
+                setColor('white');
+                setTextColor('black');
+            } else {
+                setColor('transparent');
+                setTextColor('white');
+            }
+        };
+        window.addEventListener('scroll', changeColor); 
+    }, []);
 
     const handleNav = () => {
         setNav(!nav);
 
-    }
+    };
+
+
 
 
     return ( 
-        <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div style={{backgroundColor: `${color}`}} className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
             <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
                 <Link href='/'>
-                    <h1 className='font-bold text-4xl'>Capturar</h1>
+                    <h1 style={{color:`${textColor}`}} className='font-bold text-4xl'>Capturar</h1>
                 
                 </Link>
                 <ul className='hidden sm:flex'>
@@ -37,7 +54,7 @@ const Navbar = () => {
 
                 {/* Mobile Menu */}
                 <div className='block sm:hidden z-10'>
-                    {nav ? <AiOutlineClose/> : <AiOutlineMenu size={20}  />}
+                    {nav ? <AiOutlineClose/> : <AiOutlineMenu size={20} style={{color:`${textColor}`}} />}
                 </div>
                 <div className={nav ? 'sm:hidden absolute top-0 bottom-0 right-0 left-0  justify-center items-center w-full h-screen bg-black text-center ease-in duration-300' 
                 :
